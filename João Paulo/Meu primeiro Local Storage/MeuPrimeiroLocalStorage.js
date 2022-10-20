225ed64d78b3ed09209f60596e5e250bc661979d
João Paulo <jo_paulo_19@hotmail.com>
2022-10-20 18:11:10 -0300

    att 3

--- João Paulo/Meu primeiro Local Storage/MeuPrimeiroLocalStorage.js
+++ João Paulo/Meu primeiro Local Storage/MeuPrimeiroLocalStorage.js
@@ -1,13 +1,16 @@
-let numeros = []
-let addNumero = document.getElementById("inp")
+let nomes = []
+let addNome = document.getElementById("inp")
+let excluiNome = document.getElementById("inp2")
+let editaNome = document.getElementById("inp3")
+let posiçaoIndice
 
 function Cadastra() {
 
-    numeros = JSON.parse(localStorage.getItem('Numeros'))
+    nomes = JSON.parse(localStorage.getItem('nomes'))
 
-    if (numeros === null) {
+    if (nomes === null) {
 
-        numeros = []
+        nomes = []
 
         CadastraNome()
 
@@ -20,8 +23,48 @@ function Cadastra() {
 
 function CadastraNome() {
 
-    numeros.push(addNumero.value)
-    localStorage.setItem('Numeros', JSON.stringify(numeros))
-    document.getElementById("esquerda").innerHTML = numeros
+    nomes.push(addNome.value)
+    localStorage.setItem('nomes', JSON.stringify(nomes))
+    document.getElementById("esquerda").innerHTML = nomes
+
+}
+
+function Excluir() {
+
+    nomes = JSON.parse(localStorage.getItem('nomes'))
+    posiçaoIndice = nomes.indexOf(excluiNome.value)
+    if (excluiNome.value == nomes[posiçaoIndice]) {
+
+        nomes.splice(posiçaoIndice, 1)
+        localStorage.setItem('nomes', JSON.stringify(nomes))
+        document.getElementById("esquerda").innerHTML = nomes
+
+    }
+    else {
+
+        document.getElementById("esquerda").innerHTML = (`Nome não existente`)
+
+    }
+
+}
+
+function Editar() {
+
+
+
+}
+function Pesquisar() {
+
+
+
+}
+function Listar() {
+
+
+
+}
+function Sair() {
+
+
 
 }
