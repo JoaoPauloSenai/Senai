81ad0a0b031dbd57da7e113c7f75a9a77db7ad2f
João Paulo <jo_paulo_19@hotmail.com>
2022-10-21 17:56:54 -0300

    21/10/2022

---
+++ João Paulo/Meu Primeiro Objeto/Objeto.js
@@ -0,0 +1,77 @@
+/*let livro = {
+    titulo: "jurassic Park",
+    autor: "Michael Chricton",
+    editora: "Rocco",
+    paginas: 489
+
+}
+livro.copias = 5
+livro.titulo = "Mundo Perdido"
+
+alert(`${Object.entries(livro)}\n${Object.keys(livro)}\n${Object.values(livro)}`)*/
+
+let nome = document.getElementById("inpNome")
+let senha = document.getElementById("inpSenha")
+let confSenha = document.getElementById("inpConfSenha")
+let vetor = []
+
+function CadastraUser() {
+
+    let usuario = {
+
+        username: "",
+        password: "",
+        confPassword: ""
+    }
+
+    usuario.username = nome.value
+    usuario.password = senha.value
+    usuario.confPassword = confSenha.value
+    vetor.push(usuario)
+
+    vetor = JSON.parse(localStorage.getItem('usuarioSalvo'))
+
+    if (vetor === null) {
+
+        vetor = []
+
+        vetor.push(usuario)
+        localStorage.setItem('usuarioSalvo', JSON.stringify(vetor))
+
+    } else {
+
+        vetor.push(usuario)
+        localStorage.setItem('usuarioSalvo', JSON.stringify(vetor))
+
+    }
+
+    LimpaInputs()
+
+    console.log(vetor)
+}
+
+function PesquisaUser() {
+
+    for (i = 0; i < vetor.length; i++) {
+
+        if (nome.value == vetor[i].username) {
+
+            nome.value = vetor[i].username
+            senha.value = vetor[i].password
+            confSenha.value = vetor[i].confPassword
+
+        }
+
+    }
+
+}
+
+function LimpaInputs() {
+
+    nome.value = ""
+    senha.value = ""
+    confSenha.value = ""
+
+}
+
+
--- João Paulo/Meu primeiro Local Storage/MeuPrimeiroLocalStorage.js
+++ João Paulo/Meu primeiro Local Storage/MeuPrimeiroLocalStorage.js
@@ -1,7 +1,8 @@
 let nomes = []
 let addNome = document.getElementById("inp")
 let excluiNome = document.getElementById("inp2")
-let editaNome = document.getElementById("inp3")
+let alteraNome = document.getElementById("inp3")
+let pesquisaNome = document.getElementById("inp4")
 let posiçaoIndice
 
 function Cadastra() {
@@ -45,17 +46,33 @@ function Excluir() {
         document.getElementById("esquerda").innerHTML = (`Nome não existente`)
 
     }
-
 }
 
 function Editar() {
 
+    nomes = JSON.parse(localStorage.getItem('nomes'))
+    posiçaoIndice = nomes.indexOf(pesquisaNome.value)
+    document.getElementById("esquerda").innerHTML = nomes[posiçaoIndice]
 
+        if (pesquisaNome.value == nomes[posiçaoIndice]) {
+
+        nomes.splice(posiçaoIndice, 1, alteraNome.value)
+        localStorage.setItem('nomes', JSON.stringify(nomes))
+        document.getElementById("esquerda").innerHTML = nomes
+
+    }
+    else {
+
+        document.getElementById("esquerda").innerHTML = (`Nome não existente`)
+
+    }
 
 }
 function Pesquisar() {
 
-
+    nomes = JSON.parse(localStorage.getItem('nomes'))
+    posiçaoIndice = nomes.indexOf(pesquisaNome.value)
+    document.getElementById("esquerda").innerHTML = nomes[posiçaoIndice]
 
 }
 function Listar() {
